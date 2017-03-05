7ec4b0057357b8de17f7aadb04eb3f7cdd664649
Zin Tun <ztun@pcp073972pcs.wireless.calpoly.edu>
2017-03-05 12:47:19 -0800

    assignment 3

---
+++ zin_folder/hello_world.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // The statements here will be executed when the compiled binary is called
+
+    // Print text to the console
+    println!("Hello World!");
+    let x = 5 + /* 90 + */ 2;
+     println!("Is `x` 10 or 100? x = {}", x);
+       println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
+}
